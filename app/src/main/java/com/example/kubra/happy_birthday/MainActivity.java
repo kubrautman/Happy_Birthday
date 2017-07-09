3009292e5e2dba2502cc2bc132c00d1201f15df9
utmankbra <kubrautman@hotmail.com>
2017-07-09 12:12:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kubra/happy_birthday/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kubra.happy_birthday;
+
+import android.graphics.drawable.AnimatedStateListDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import  android.widget.ListView;
+import  android.widget.TextView;
+import  android.view.View;
+import  android.view.ViewGroup;
+import  android.view.LayoutInflater;
+import  android.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
